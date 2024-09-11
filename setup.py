15aee5d6d64cea485fbf65c5b883bb290f977161
XtraF <xtraf@xtrafpc.localdomain>
2024-09-11 23:30:58 +0400

    v 2.0

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 
 def readme():
-    with open('README.md', 'r') as f:
+    with open('README.md', 'r', encoding="utf-8") as f:
         return f.read()
 
 
 setup(
     name='PyCharacterAI',
-    version='1.1.0',
+    version='2.0.0',
     author='XtraF',
     author_email='igoromarov15@gmail.com',
     description='An unofficial asynchronous api wrapper for Character AI.',
@@ -16,11 +16,16 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/Xtr4F/PyCharacterAI',
     packages=find_packages(),
-    install_requires=['curl-cffi==0.5.9'],
+    install_requires=['websockets',
+                      'curl-cffi'],
     classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
         'License :: OSI Approved :: MIT License'
     ],
-    python_requires='>=3.7'
+    python_requires='>=3.8'
 )
 
