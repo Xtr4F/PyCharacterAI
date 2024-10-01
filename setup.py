5603b6e0f18922d0454774aecd8ffd14e3ed85a6
XtraF <xtraf@xtrafpc.localdomain>
2024-10-01 16:23:03 +0400

    curl_cffi downgraded due to bug with POST requests

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url='https://github.com/Xtr4F/PyCharacterAI',
     packages=find_packages(),
     install_requires=[
-        'curl-cffi',
+        'curl-cffi==0.7.1',
         'aiohttp'
     ],
     classifiers=[
